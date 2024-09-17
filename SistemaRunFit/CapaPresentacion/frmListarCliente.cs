d59047e10eb19c9016ef649e96f23d7482acdf2d
Julio Osvaldo Canteros <radamanthys256@gmail.com>
2024-09-17 18:36:20 -0300

    act

--- SistemaRunFit/CapaPresentacion/frmListarCliente.cs
+++ SistemaRunFit/CapaPresentacion/frmListarCliente.cs
@@ -19,7 +19,7 @@ namespace CapaPresentacion
         public frmListarCliente(Inicio frminicio)
         {
             InitializeComponent();
-            this.Load += new EventHandler(frmListarCliente_Load);
+            //this.Load += new EventHandler(frmListarCliente_Load);
             _inicio = frminicio;
             frminicio.PnlContenedorMenu.Enabled= false;
         }
