5aff655d6045d7f0feedf4448ded247fe322a853
Matias <mati.j.martinez@gmail.com>
2024-09-19 20:05:45 -0300

    BD Act

--- modeloBD_RunFit.sql
+++ modeloBD_RunFit.sql
@@ -179,9 +179,8 @@ insert USUARIOS(nombre_usuario,password,id_persona,id_rol) values('julio', '123'
  GO
 
 
--- Prodcedimientos almacenados
- go
---/*REGISTRAR PERSONA*/
+--Prodcedimientos almacenados
+--REGISTRAR PERSONA
  CREATE PROC SP_REGISTRARPERSONA(
     @dni VARCHAR(20),
     @nombre VARCHAR(50),
@@ -302,6 +301,69 @@ insert USUARIOS(nombre_usuario,password,id_persona,id_rol) values('julio', '123'
 	set @Mensaje = 'Domicilio actualizado'
  end
  go
+
+ /* REGISTRAR USUARIO*/
+ CREATE PROC SP_REGISTRARUSUARIO(
+    @nombre_usuario VARCHAR(20),
+    @password VARCHAR(255),
+    @id_persona INT,
+    @id_rol INT,
+	@IdUsuarioResultado int output,
+	@Mensaje VARCHAR(500) output 
+ )
+ as
+ begin
+	set @IdUsuarioResultado = 0
+	set @Mensaje = ''
+
+	if not exists (SELECT * from USUARIOS WHERE nombre_usuario = @nombre_usuario)
+	begin
+		insert USUARIOS(nombre_usuario,password,id_persona,id_rol) values(@nombre_usuario,@password,@id_persona,@id_rol)
+
+		set @IdUsuarioResultado = SCOPE_IDENTITY()
+		
+	end
+	else
+		set @Mensaje = 'Ya existe un EMPLEADO con el mismo nombre de usuario. Ingrese un nombre diferente'
+
+ end
+ go
+
+ /*EDITAR USUARIO*/
+ CREATE PROC SP_EDITARUSUARIO(
+	@id_usuario INT,
+	@nombre_usuario VARCHAR(20),
+    @password VARCHAR(255),
+	@fecha_baja DATE = NULL,
+    @id_persona INT,
+    @id_rol INT,
+	@Respuesta bit output,
+	@Mensaje VARCHAR(500) output 
+ )
+ as
+ begin
+	set @Respuesta = 0
+	set @Mensaje = ''
+
+	if not exists (SELECT * from USUARIOS WHERE nombre_usuario = @nombre_usuario and id_usuario != @id_usuario)
+	begin
+		update USUARIOS set
+		nombre_usuario = @nombre_usuario,
+		password = @password,
+		fecha_baja = @fecha_baja,
+		id_persona = @id_persona,
+		id_rol = @id_rol
+		WHERE id_usuario = @id_usuario
+
+		set @Respuesta = 1
+		set @Mensaje = 'Usuario REGISTRADO'
+		
+	end
+	else
+		set @Mensaje = 'Ya existe un EMPLEADO con el mismo nombre de usuario. Ingrese un nombre diferente'
+
+ end
+ go
  --ELIMINAR USUARIO
  CREATE PROC SP_ELIMINARUSUARIO
 (
@@ -387,13 +449,3 @@ END
 
  GO
 
-
-
-
- select * from USUARIOS
- select * from PERSONAS
- SELECT d.id_domicilio, d.calle, d.altura, d.casa, d.manzana, d.departamento, d.piso, d.id_domicilio, d.id_persona,
- p.dni, p.nombre, p.apellido, p.email, p.telefono, p.fecha_nacimiento, p.sexo
- FROM DOMICILIOS d
- inner join PERSONAS p on p.id_persona = d.id_persona
- WHERE d.id_persona = p.id_persona
