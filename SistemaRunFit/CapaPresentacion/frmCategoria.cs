8ab40d864e5282ad86bf4423e1f8031e7447727b
Matias <mati.j.martinez@gmail.com>
2024-10-15 20:27:14 -0300

    detalles vizuales Marca|Categoria|Productos

--- SistemaRunFit/CapaPresentacion/frmCategoria.cs
+++ SistemaRunFit/CapaPresentacion/frmCategoria.cs
@@ -15,16 +15,13 @@ namespace CapaPresentacion
 {
     public partial class frmCategoria : Form
     {
-        // Variable para almacenar la referencia del formulario Inicio.
         Inicio _inicio;
 
-        // Constructor de frmCategoria que recibe una instancia de Inicio.
         public frmCategoria(Inicio inicio)
         {
             InitializeComponent(); 
             _inicio = inicio; 
             _inicio.PnlContenedorMenu.Enabled = false; 
-           
         }
         private void Listar_Categorias()
         {
@@ -32,80 +29,68 @@ namespace CapaPresentacion
             foreach (Categoria item in ListaCategoria)
             {
                 dgvCategoria.Rows.Add(new object[] {
-            CapaPresentacion.Properties.Resources.pencil, // Icono de editar
-            item.fecha_baja == null ? CapaPresentacion.Properties.Resources.eliminar_categoria: CapaPresentacion.Properties.Resources.activar_categoria, // Icono de acción
+            CapaPresentacion.Properties.Resources.pencil,
+            item.fecha_baja == null ? CapaPresentacion.Properties.Resources.eliminar_categoria: CapaPresentacion.Properties.Resources.activar_categoria,
             item.idCategoria,
             item.fecha_baja == null ? "Activo" : "Inactivo", 
             item.nombre_categoria,
-           // item.fecha_alta,
-            
-            
                  });
             }
 
-            // Cambia el color del texto según el estado de la categoria (Activo/Inactivo)
             for (int i = 0; i < dgvCategoria.Rows.Count; i++)
             {
-                // Obtener el valor de la celda "Estado" en la fila actual
                 string estado = dgvCategoria.Rows[i].Cells["Estado"].Value.ToString();
 
-                // Cambiar el color tanto para cuando la fila está seleccionada como cuando no lo está
                 if (estado == "Activo")
                 {
-                    // Cambiar el color del texto a negro si es "Activo"
                     dgvCategoria.Rows[i].Cells["Estado"].Style.ForeColor = Color.Black;
-                    dgvCategoria.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Black; // También cuando está seleccionada
+                    dgvCategoria.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Black;
                 }
                 else if (estado == "Inactivo")
                 {
-                    // Cambiar el color del texto a rojo si es "Inactivo"
                     dgvCategoria.Rows[i].Cells["Estado"].Style.ForeColor = Color.Red;
-                    dgvCategoria.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Red; // También cuando está seleccionada
+                    dgvCategoria.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Red;
                 }
             }
         }
         private void LimpiarCampos()
         {
             txtCategoria.Clear();
+            txtBuscarCategoria.Clear();
+            txtIdCategoria.Clear();
         }
-        // Evento que se ejecuta al hacer clic en el botón btnGuardarCategoria.
         private void btnGuardarCategoria_Click(object sender, EventArgs e)
         {
-            string MensajeCategoria = string.Empty; // Mensaje para el resultado del proceso de usuario
-            int IdCategoriaGenerada = 0; // ID de persona generada
+            string MensajeCategoria = string.Empty;
+            int IdCategoriaGenerada = 0;
             bool VerdadCategoriaGenerada = false;
-
-            // bool VerdadUsuarioGenerado = false; // Bandera para verificar si el usuario fue generado correctamente
-            string mensajeConfirmacion = "¿Desea agregar al"; // Mensaje de confirmación inicial
+            string mensajeConfirmacion = "¿Desea agregar la categoria";
             
-
-            // Verifica si el campo de texto txtCategoria no está vacío.
             if (!string.IsNullOrEmpty(txtCategoria.Text))
             {
-                // Muestra un cuadro de diálogo para confirmar la acción de agregar la categoría.
+                if (!string.IsNullOrEmpty(txtIdCategoria.Text))
+                {
+                    mensajeConfirmacion = "¿Modificar la categoria a";
+                }
                 var confirmacion = MessageBox.Show(
-                    $"Desea agregar la categoria {txtCategoria.Text} ?",
+                    $"{mensajeConfirmacion} {txtCategoria.Text} ?",
                     "Confirmación",
                     MessageBoxButtons.YesNo,
                     MessageBoxIcon.Question
                 );
 
-                // Si el usuario confirma, se procede a guardar los datos.
                 if (confirmacion == DialogResult.Yes)
                 {
                     Categoria CategoriaNueva = new Categoria()
                     {
                         idCategoria = txtIdCategoria.Text != "" ? Convert.ToInt32(txtIdCategoria.Text) : IdCategoriaGenerada,
                         nombre_categoria = txtCategoria.Text,
-                        
                     };
 
-                    // Si hay un ID de categoria, se edita
                     if (txtIdCategoria.Text != "")
                     {
                         VerdadCategoriaGenerada = new CN_Categoria().Editar(CategoriaNueva, out MensajeCategoria);
-                        IdCategoriaGenerada = CategoriaNueva.idCategoria; // Actualiza el ID de categoria generada
-                       
+                        IdCategoriaGenerada = CategoriaNueva.idCategoria;
                     }
                     else
                     {
@@ -119,40 +104,33 @@ namespace CapaPresentacion
                     }
                     else
                     {
-                        // Muestra mensajes de error si no se guardaron los datos
                         MessageBox.Show(MensajeCategoria );
                         txtIdCategoria.Text = "";
                     }
                     
-                    // Borrar cuando se integre la BD
-                    txtCategoria.Clear();
+                    LimpiarCampos();
                     dgvCategoria.Rows.Clear();
                     Listar_Categorias();
-                    txtCategoria.Focus(); 
+                    txtCategoria.Clear();
+                    txtCategoria.Focus();
+                    lblCategoria.Text = "Nueva Categoria";
+                    btnGuardarCategoria.Text = "Guardar";
                 }
             }
             else
             {
-                // Muestra un mensaje de error si la categoría está vacía.
                 MessageBox.Show("Categoria vacia!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
-        
-        // Evento que se ejecuta al presionar una tecla en el campo txtCategoria.
         private void txtCategoria_KeyPress(object sender, KeyPressEventArgs e)
         {
-            // Permite solo letras, controles y espacios en blanco.
             if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
             {
                 e.Handled = true; 
             }
         }
-
-        // Evento que se ejecuta al presionar una tecla en el campo txtBuscarCategoria.
         private void txtBuscarCategoria_KeyPress(object sender, KeyPressEventArgs e)
         {
-            // Permite solo letras, controles y espacios en blanco.
             if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
             {
                 e.Handled = true; 
@@ -164,21 +142,17 @@ namespace CapaPresentacion
                 {
                     
                     if (row.Cells["Categoria"].Value.ToString().Trim().ToUpper().Contains(txtBuscarCategoria.Text.Trim().ToUpper()))
-                        row.Visible = true; // Muestra la fila si coincide
+                        row.Visible = true;
                     else
-                        row.Visible = false; // Oculta la fila si no coincide
+                        row.Visible = false;
                 }
             }
         }
-
-        // Evento que se ejecuta al cargar el formulario.
         private void frmCategoria_Load_1(object sender, EventArgs e)
         {
             txtCategoria.Focus();
             Listar_Categorias();
         }
-
-        // Evento que se ejecuta al hacer clic en el botón btnMenuMarca.
         private void btnMenuMarca_Click(object sender, EventArgs e)
         {
             if (_inicio != null) 
@@ -193,8 +167,8 @@ namespace CapaPresentacion
         {
             if (dgvCategoria.Columns[e.ColumnIndex].Name == "Editar")
             {
-                int n = e.RowIndex; // Obtiene el índice de la fila seleccionada
-                if (n >= 0) // Verifica que el índice sea válido
+                int n = e.RowIndex;
+                if (n >= 0)
                 {
                     
                     Categoria CategoriaEditar = new Categoria()
@@ -203,74 +177,65 @@ namespace CapaPresentacion
                         nombre_categoria = dgvCategoria.Rows[n].Cells["categoria"].Value.ToString()
                     };
 
-                   txtCategoria.Text= CategoriaEditar.nombre_categoria;
+                    txtCategoria.Text= CategoriaEditar.nombre_categoria;
                     txtIdCategoria.Text = CategoriaEditar.idCategoria.ToString();
-                   // btnGuardarCategoria.Text = "Actualizar";
+                    lblCategoria.Text = "Editar Categoria";
+                    btnGuardarCategoria.Text = "Actualizar";
+                    dgvCategoria.Rows.Clear();
+                    Listar_Categorias();
 
                 }
-                //txtIdCategoria.Text = "";
             }
 
-            // Verifica si la columna seleccionada es la de "Accion"
             if (dgvCategoria.Columns[e.ColumnIndex].Name == "Eliminar")
             {
-                int n = e.RowIndex; // Obtiene el índice de la fila seleccionada
-                if (n >= 0) // Verifica que el índice sea válido
+                int n = e.RowIndex;
+                if (n >= 0)
                 {
-                    string mensaje = string.Empty; // Mensaje para la acción
-                    Categoria CategoriaEliminar = new Categoria() // Crea un objeto Usuario para la acción
+                    string mensaje = string.Empty;
+                    Categoria CategoriaEliminar = new Categoria()
                     {
-                        idCategoria = (int)dgvCategoria.Rows[n].Cells["ID_Categoria"].Value // Asigna el ID del usuario
+                        idCategoria = (int)dgvCategoria.Rows[n].Cells["ID_Categoria"].Value
                     };
 
                     string estadoActual = dgvCategoria.Rows[n].Cells["Estado"].Value.ToString(); 
                     string categoria = dgvCategoria.Rows[n].Cells["Categoria"].Value.ToString(); 
 
-                    // Si la categoria está inactiva, se le pregunta si desea activarlo
                     if (estadoActual == "Inactivo")
                     {
                         DialogResult ask = MessageBox.Show($"¿Desea ACTIVAR la categoria {categoria}?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                         if (ask == DialogResult.Yes)
                         {
-                            // Llama al procedimiento almacenado para activar la categoria
                             bool respuesta = new CN_Categoria().Eliminar(CategoriaEliminar, out mensaje);
 
                             if (respuesta)
                             {
-                                // Actualizar el estado de la categoria
                                 dgvCategoria.Rows[n].Cells["Estado"].Value = "Activo";
-                                dgvCategoria.Rows[n].Cells["Estado"].Style.ForeColor = Color.Black; // Cambia el color a negro
-                                dgvCategoria.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Black; // Cambia el color de selección a negro
-
+                                dgvCategoria.Rows[n].Cells["Estado"].Style.ForeColor = Color.Black;
+                                dgvCategoria.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Black;
                                 MessageBox.Show("Categoria " + categoria + " activada correctamente", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                             }
                         }
                     }
-                    else // Si la categoria está activo, se le pregunta si desea desactivarlo
+                    else
                     {
                         DialogResult ask = MessageBox.Show($"¿Desea DESACTIVAR la categoria {categoria}?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                         if (ask == DialogResult.Yes)
                         {
-                            // Llama al procedimiento almacenado para desactivar la categoria
                             bool respuesta = new CN_Categoria().Eliminar(CategoriaEliminar, out mensaje);
-
-                            // Actualizar el estado del usuario
                             dgvCategoria.Rows[n].Cells["Estado"].Value = "Inactivo";
-                            dgvCategoria.Rows[n].Cells["Estado"].Style.ForeColor = Color.Red; // Cambia el color a rojo
-                            dgvCategoria.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Red; // Cambia el color de selección a rojo
-
+                            dgvCategoria.Rows[n].Cells["Estado"].Style.ForeColor = Color.Red;
+                            dgvCategoria.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Red;
                             MessageBox.Show("Categoria " + categoria + " desactivada correctamente", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                         }
                     }
-
-                    // Limpia las filas del DataGridView y vuelve a cargar los datos
-                    dgvCategoria.Rows.Clear(); // Limpia el DataGridView
-                    Listar_Categorias(); // Llama a la función para volver a cargar los datos
+                    dgvCategoria.Rows.Clear();
+                    Listar_Categorias();
                 }
             }
         }
     }
-    }
+}
 
 
 
--- SistemaRunFit/CapaPresentacion/frmMarca.cs
+++ SistemaRunFit/CapaPresentacion/frmMarca.cs
@@ -14,15 +14,12 @@ namespace CapaPresentacion
 {
     public partial class frmMarca : Form
     {
-        // Variable privada que almacena la referencia al formulario principal (Inicio)
         Inicio _inicio;
-
-        // Constructor de la clase frmMarca
         public frmMarca(Inicio inicio)
         {
-            InitializeComponent(); // Inicializa los componentes del formulario
-            _inicio = inicio; // Asigna la referencia del formulario principal a la variable _inicio
-            _inicio.PnlContenedorMenu.Enabled = false; // Desactiva el panel de menú en el formulario principal
+            InitializeComponent();
+            _inicio = inicio;
+            _inicio.PnlContenedorMenu.Enabled = false;
            
         }
         private void Listar_Marcas()
@@ -31,99 +28,85 @@ namespace CapaPresentacion
             foreach (Marca item in ListaMarca)
             {
                 dgvMarca.Rows.Add(new object[] {
-            CapaPresentacion.Properties.Resources.pencil, // Icono de editar
-            item.fechaBaja == null ? CapaPresentacion.Properties.Resources.eliminar_marca: CapaPresentacion.Properties.Resources.activar_marca, // Icono de acción
+            CapaPresentacion.Properties.Resources.pencil,
+            item.fechaBaja == null ? CapaPresentacion.Properties.Resources.eliminar_marca: CapaPresentacion.Properties.Resources.activar_marca,
             item.idMarca,
             item.fechaBaja == null ? "Activo" : "Inactivo",
             item.nombre,
-           // item.fecha_alta,
-            
-            
                  });
             }
 
-            // Cambia el color del texto según el estado de la Marca (Activo/Inactivo)
             for (int i = 0; i < dgvMarca.Rows.Count; i++)
             {
-                // Obtener el valor de la celda "Estado" en la fila actual
                 string estado = dgvMarca.Rows[i].Cells["Estado"].Value.ToString();
 
-                // Cambiar el color tanto para cuando la fila está seleccionada como cuando no lo está
                 if (estado == "Activo")
                 {
-                    // Cambiar el color del texto a negro si es "Activo"
                     dgvMarca.Rows[i].Cells["Estado"].Style.ForeColor = Color.Black;
-                    dgvMarca.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Black; // También cuando está seleccionada
+                    dgvMarca.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Black;
                 }
                 else if (estado == "Inactivo")
                 {
-                    // Cambiar el color del texto a rojo si es "Inactivo"
                     dgvMarca.Rows[i].Cells["Estado"].Style.ForeColor = Color.Red;
-                    dgvMarca.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Red; // También cuando está seleccionada
+                    dgvMarca.Rows[i].Cells["Estado"].Style.SelectionForeColor = Color.Red;
                 }
             }
         }
-        // Evento que se ejecuta al cargar el formulario
         private void frmMarca_Load(object sender, EventArgs e)
         {
-            txtBuscarMarca.Focus(); // Pone el foco en el TextBox de búsqueda de marcas
+            txtMarca.Focus();
             Listar_Marcas();
         }
 
-        // Evento que se ejecuta al hacer clic en el botón de menú de marcas
         private void btnMenuMarca_Click(object sender, EventArgs e)
         {
-            // Verifica si la variable _inicio no es nula
             if (_inicio != null)
             {
-                // Reactiva el panel en el formulario de inicio
                 _inicio.PnlContenedorMenu.Enabled = true;
-                // Muestra la imagen de fondo en el formulario de inicio
                 _inicio.MostrarImagenFondo();
             }
-            // Cierra el formulario actual
             this.Close();
         }
         private void LimpiarCampos()
         {
             txtMarca.Clear();
+            txtBuscarMarca.Clear();
+            txtIdMarca.Clear();
         }
         private void btnGuardarMarca_Click(object sender, EventArgs e)
         {
-
             string MensajeMarca = string.Empty;
             int IdMarcaGenerada = 0;
             bool VerdadMarcaGenerada = false;
-            string mensajeConfirmacion = "¿Desea agregar al";
+            string mensajeConfirmacion = "¿Desea agregar la marca";
 
 
             if (!string.IsNullOrEmpty(txtMarca.Text))
             {
+                if (!string.IsNullOrEmpty(txtIdMarca.Text))
+                {
+                    mensajeConfirmacion = "¿Modificar la marca a";
+                }
+
                 var confirmacion = MessageBox.Show(
-                    $"Desea agregar la categoria {txtMarca.Text} ?",
+                    $"{mensajeConfirmacion} {txtMarca.Text} ?",
                     "Confirmación",
                     MessageBoxButtons.YesNo,
                     MessageBoxIcon.Question
                 );
 
-                // Si el usuario confirma, se procede a guardar los datos.
                 if (confirmacion == DialogResult.Yes)
                 {
                     Marca MarcaNueva = new Marca()
                     {
-
                         idMarca = txtIdMarca.Text != "" ? Convert.ToInt32(txtIdMarca.Text) : IdMarcaGenerada,
                         nombre = txtMarca.Text.ToString(),
-
-
                     };
 
                     if (txtIdMarca.Text != "")
                     {
                         VerdadMarcaGenerada = new CN_Marca().Editar(MarcaNueva, out MensajeMarca);
-
                         IdMarcaGenerada = MarcaNueva.idMarca; 
-
                     }
                     else
                     {
@@ -140,16 +123,16 @@ namespace CapaPresentacion
                         txtIdMarca.Text = "";
                     }
 
-                    // Borrar cuando se integre la BD
-                    txtMarca.Clear();
+                    LimpiarCampos();
                     dgvMarca.Rows.Clear();
                     Listar_Marcas();
-                     txtMarca.Focus(); 
+                    txtMarca.Focus();
+                    lblNuevaMarca.Text = "Nueva Marca";
+                    btnGuardarMarca.Text = "Guardar";
                 }
             }
             else
             {
-                // Muestra un mensaje de error si la categoría está vacía.
                 MessageBox.Show("Categoria vacia!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
@@ -158,8 +141,8 @@ namespace CapaPresentacion
         {
             if (dgvMarca.Columns[e.ColumnIndex].Name == "Editar")
             {
-                int n = e.RowIndex; // Obtiene el índice de la fila seleccionada
-                if (n >= 0) // Verifica que el índice sea válido
+                int n = e.RowIndex;
+                if (n >= 0)
                 {
 
                     Marca MarcaEditar = new Marca()
@@ -170,19 +153,21 @@ namespace CapaPresentacion
 
                     txtMarca.Text = MarcaEditar.nombre;
                     txtIdMarca.Text = MarcaEditar.idMarca.ToString();
-                    // btnGuardarCategoria.Text = "Actualizar";
+                    lblNuevaMarca.Text = "Editar Marca";
+                    btnGuardarMarca.Text = "Actualizar";
+                    dgvMarca.Rows.Clear();
+                    Listar_Marcas();
 
                 }
-                //txtIdCategoria.Text = "";
             }
-            // Verifica si la columna seleccionada es la de "Accion"
+
             if (dgvMarca.Columns[e.ColumnIndex].Name == "Eliminar")
             {
-                int n = e.RowIndex; // Obtiene el índice de la fila seleccionada
-                if (n >= 0) // Verifica que el índice sea válido
+                int n = e.RowIndex;
+                if (n >= 0)
                 {
-                    string mensaje = string.Empty; // Mensaje para la acción
-                    Marca MarcaEliminar = new Marca() // Crea un objeto marca para la acción
+                    string mensaje = string.Empty;
+                    Marca MarcaEliminar = new Marca()
                     {
                         idMarca = (int)dgvMarca.Rows[n].Cells["ID_marca"].Value 
                     };
@@ -190,46 +175,37 @@ namespace CapaPresentacion
                     string estadoActual = dgvMarca.Rows[n].Cells["Estado"].Value.ToString();
                     string marca = dgvMarca.Rows[n].Cells["nombre_marca"].Value.ToString();
 
-                    // Si la Marca está inactiva, se le pregunta si desea activarlo
                     if (estadoActual == "Inactivo")
                     {
                         DialogResult ask = MessageBox.Show($"¿Desea ACTIVAR la marca {marca}?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                         if (ask == DialogResult.Yes)
                         {
-                            // Llama al procedimiento almacenado para activar la marca
                             bool respuesta = new CN_Marca().Eliminar(MarcaEliminar, out mensaje);
 
                             if (respuesta)
                             {
-                                // Actualizar el estado de la categoria
                                 dgvMarca.Rows[n].Cells["Estado"].Value = "Activo";
-                                dgvMarca.Rows[n].Cells["Estado"].Style.ForeColor = Color.Black; // Cambia el color a negro
-                                dgvMarca.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Black; // Cambia el color de selección a negro
-
+                                dgvMarca.Rows[n].Cells["Estado"].Style.ForeColor = Color.Black;
+                                dgvMarca.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Black;
                                 MessageBox.Show("Marca " + marca + " activada correctamente", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                             }
                         }
                     }
-                    else // Si la Marca está activo, se le pregunta si desea desactivarlo
+                    else
                     {
                         DialogResult ask = MessageBox.Show($"¿Desea DESACTIVAR la marca {marca}?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                         if (ask == DialogResult.Yes)
                         {
-                            // Llama al procedimiento almacenado para desactivar la categoria
                             bool respuesta = new CN_Marca().Eliminar(MarcaEliminar, out mensaje);
-
-                            // Actualizar el estado del usuario
                             dgvMarca.Rows[n].Cells["Estado"].Value = "Inactivo";
-                            dgvMarca.Rows[n].Cells["Estado"].Style.ForeColor = Color.Red; // Cambia el color a rojo
-                            dgvMarca.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Red; // Cambia el color de selección a rojo
-
+                            dgvMarca.Rows[n].Cells["Estado"].Style.ForeColor = Color.Red;
+                            dgvMarca.Rows[n].Cells["Estado"].Style.SelectionForeColor = Color.Red;
                             MessageBox.Show("Marca " + marca + " desactivada correctamente", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                         }
                     }
 
-                    // Limpia las filas del DataGridView y vuelve a cargar los datos
-                    dgvMarca.Rows.Clear(); // Limpia el DataGridView
-                    Listar_Marcas(); // Llama a la función para volver a cargar los datos
+                    dgvMarca.Rows.Clear();
+                    Listar_Marcas();
                 }
             }
         }
@@ -246,9 +222,9 @@ namespace CapaPresentacion
                 {
                     
                     if (row.Cells["nombre_marca"].Value.ToString().Trim().ToUpper().Contains(txtBuscarMarca.Text.Trim().ToUpper()))
-                        row.Visible = true; // Muestra la fila si coincide
+                        row.Visible = true;
                     else
-                        row.Visible = false; // Oculta la fila si no coincide
+                        row.Visible = false;
                 }
             }
         }
